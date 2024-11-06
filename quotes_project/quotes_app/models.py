8411faefc278e7946737311f86b39a00ef7f9f0e
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2024-11-06 23:09:00 +0200

    Create models.py

---
+++ quotes_project/quotes_app/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+#import uuid
+
+class Author(models.Model):
+    fullname = models.CharField(max_length=255)
+    born_date = models.CharField(max_length=50)
+    born_location = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.fullname
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Quote(models.Model):
+    quote = models.TextField()
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.quote
