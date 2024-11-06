54ae101a3b26f8dc7a9a1c93f6838beca8de1228
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2024-11-06 23:27:16 +0200

    Create convert_to_utf8.py

---
+++ quotes_project/utils/convert_to_utf8.py
@@ -0,0 +1,20 @@
+﻿from django.conf import settings
+import chardet
+import json
+import os
+
+
+# This script formats .json file in utf-8
+
+# ?.json - This is any file name ( ? )
+file_path = os.path.join(settings.BASE_DIR, ' ', '?.json')
+
+with open('?.json', 'rb') as file:
+    raw_data = file.read()
+    encoding = chardet.detect(raw_data)['encoding']
+
+with open('?.json', 'r', encoding=encoding) as file:
+    data = json.load(file)
+
+with open('?.json', 'w', encoding='utf-8') as file:
+    json.dump(data, file, ensure_ascii=False, indent=4)
